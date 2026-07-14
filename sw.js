e7aa02b827a3daf22b69b822e5621382a85da6e0
caesr-code <dungeonsndumbins@gmail.com>
2026-07-14 12:00:27 +1000

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-// V8 deliberately avoids caching while collision fixes are being tested.
-self.addEventListener('install', event => self.skipWaiting());
-self.addEventListener('activate', event => event.waitUntil((async()=>{for(const k of await caches.keys())await caches.delete(k);await self.clients.claim()})()));
-self.addEventListener('fetch', event => { event.respondWith(fetch(event.request, {cache:'no-store'}).catch(()=>new Response('Offline unavailable during V8 testing',{status:503}))) });
