5ad4a4bb9998f69247ec3811ce655c8c61d379b0
caesr-code <dungeonsndumbins@gmail.com>
2026-07-14 11:52:38 +1000

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='rocket-arena-magazine-wallfix-v7';
-self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html'])))});
-self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',event=>{if(event.request.mode==='navigate'){event.respondWith(fetch(event.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return r}).catch(()=>caches.match('./index.html')));return}event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)))});
