b217efbba325f2a35b80f37911f804bc37e7ae1a
caesr-code <dungeonsndumbins@gmail.com>
2026-07-14 10:28:30 +1000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+const CACHE='rocket-arena-v99';
+const CORE=['./','./index.html','./manifest.webmanifest'];
+const THREE='https://cdn.jsdelivr.net/npm/three@0.159.0/build/three.min.js';
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(async cache=>{await cache.addAll(CORE);try{await cache.add(THREE)}catch(_){}}).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match('./index.html'))))});
