4d99d18764080737bf4aab9345b491e727a78e05
caesr-code <dungeonsndumbins@gmail.com>
2026-07-14 13:15:32 +1000

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('activate',e=>e.waitUntil((async()=>{for(const k of await caches.keys())await caches.delete(k);await self.clients.claim()})()));
-self.addEventListener('fetch',()=>{});
